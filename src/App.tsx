42bbcb1d7b731dcc52ed9e397ae863146f43cb7a
shibest <eerkingtheawesome@gmail.com>
2025-10-18 12:29:13 -0700

    move userwatchlist to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Callback from './components/Callback';
-import UserWatchlist from './components/UserWatchlist';
+import UserWatchlist from './pages/UserWatchlist';
 import ProfileSelect from './pages/ProfileSelect';
 import './App.css'
 
